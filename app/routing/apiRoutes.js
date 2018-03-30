245e137c7cc766af51877d81cfadb1c16057125d
CarL Liu <thecarlliu99@CarLs-MacBook-Air.local>
2018-03-30 12:20:22 -0500

    Finished findMatch() function

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -18,6 +18,22 @@ module.exports = function(app) {
 
 //Compares user's answers to list of scots, determines most compatible, displays in modal pop-up.
 function findMatch(userChoices, listScots) {
+    var userChoices = userChoices.split(",");
+    var highScore = 0;
     var match;
+
+    for (var i=0;i<listScots.length;i++) { //for each scot
+        var scotChoices = listScots[i].responses.split(",");
+        var currScore = 0;
+        for (var j=0;j<userChoices.length;j++) { //for each of the user's answers, compare user answer to respective scot answer
+            if (parseInt(userChoices[j]) === parseInt(scotChoices[j])) { //if they are the same,
+                currScore++; //increment currScore
+            }
+        }
+        if (currScore >= highScore) { //if the match score is higher than or equal to the high score,
+            highScore = currScore; //update high score
+            match = listScots[i]; //update match
+        }
+    }
     return match;
 }
